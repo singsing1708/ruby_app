ffe6407ec30ef4f8803990b07b8833c8db779b51
Steven Wong <sing1708@gmail.com>
2021-08-29 22:30:27 +0100

    add page_journey function

--- lib/memory_parser.rb
+++ lib/memory_parser.rb
@@ -1,6 +1,6 @@
 class MemoryParser
 
-  attr_reader :filename, :most_page_view, :uniq_most_page_view
+  attr_reader :filename, :most_page_view, :uniq_most_page_view, :page_journey
 
   def initialize(filename:)
     raise StandardError, "File does not exists" unless File.exist?(filename)
@@ -8,6 +8,7 @@ class MemoryParser
     @filename = filename
     @most_page_view = {}
     @uniq_most_page_view = {}
+    @page_journey = {}
 
     process_file
   end
@@ -19,22 +20,38 @@ class MemoryParser
 
       url, ip = result
 
-      if most_page_view[url].nil?
-        most_page_view[url] = 1
-      else
-        most_page_view[url] += 1
-      end
-
-      if uniq_most_page_view[url].nil?
-        uniq_most_page_view[url] = { :count => 1, :ips => [ip] }
-      elsif !uniq_most_page_view[url][:ips].include?(ip)
-        uniq_most_page_view[url][:count] += 1
-        uniq_most_page_view[url][:ips] << ip
-      end
+      process_most_page_view(url)
+      process_uniq_most_page_view(url, ip)
+      process_page_journey(url, ip)
     end
-    
+
     @most_page_view = most_page_view.sort_by {|key, value| [-value, key]}
     @uniq_most_page_view = uniq_most_page_view.sort_by{|key, value| [-value[:count], key]}
+  end
+
+  def process_most_page_view(url)
+    if most_page_view[url].nil?
+      most_page_view[url] = 1
+    else
+      most_page_view[url] += 1
+    end
+  end
+
+  def process_uniq_most_page_view(url, ip)
+    if uniq_most_page_view[url].nil?
+      uniq_most_page_view[url] = { :count => 1, :ips => [ip] }
+    elsif !uniq_most_page_view[url][:ips].include?(ip)
+      uniq_most_page_view[url][:count] += 1
+      uniq_most_page_view[url][:ips] << ip
+    end
+  end
+
+  def process_page_journey(url, ip)
+    if page_journey[ip].nil?
+      page_journey[ip] = [url]
+    else
+      page_journey[ip] << url
+    end
   end
 
 end
--- spec/memory_parser_spec.rb
+++ spec/memory_parser_spec.rb
@@ -2,10 +2,13 @@ require_relative "../lib/memory_parser"
 
 RSpec.describe 'MemoryParser' do
 
+  before(:all) do
+   @memory_parser = MemoryParser.new(filename: "./spec/fixtures/test_webserver.log")
+  end
+
   describe "#initalize" do
     it "should success" do
-      memory_parser = MemoryParser.new(filename: "./spec/fixtures/test_webserver.log")
-      expect(memory_parser.filename).to eq "./spec/fixtures/test_webserver.log"
+      expect(@memory_parser.filename).to eq "./spec/fixtures/test_webserver.log"
     end
 
     it "should raise error if file not exists" do
@@ -15,8 +18,7 @@ RSpec.describe 'MemoryParser' do
 
   describe "#most_page_view" do
     it "should return the desc order of page view" do
-      memory_parser = MemoryParser.new(filename: "./spec/fixtures/test_webserver.log")
-      result = memory_parser.most_page_view
+      result = @memory_parser.most_page_view
       expect(result[0]).to eq ["/help_page/1", 6]
       expect(result[1]).to eq ["/contact", 3]
       expect(result[2]).to eq ["/about/2", 1]
@@ -25,14 +27,25 @@ RSpec.describe 'MemoryParser' do
 
   describe "#uniq_most_page_view" do
     it "should return the desc order of uniq page view" do
-      memory_parser = MemoryParser.new(filename: "./spec/fixtures/test_webserver.log")
-      result = memory_parser.uniq_most_page_view
+      result = @memory_parser.uniq_most_page_view
       expect(result[0][0]).to eq "/help_page/1"
       expect(result[0][1][:count]).to eq 5
       expect(result[1][0]).to eq "/contact"
       expect(result[1][1][:count]).to eq 2
       expect(result[2][0]).to eq "/about/2"
       expect(result[2][1][:count]).to eq 1
+    end
+  end
+
+  describe "#page_journey" do
+    it "should return the page journey for the same ip" do
+      memory_parser = MemoryParser.new(filename: "./spec/fixtures/test_page_journey_webserver.log")
+      result = memory_parser.page_journey
+
+      expect(result["929.398.951.881"]).to eq ["/help_page/1", "/help_page/2", "/help_page/3"]
+      expect(result["929.398.951.889"]).to eq ["/help_page/1"]
+      expect(result["444.701.448.104"]).to eq ["/index", "/about"]
+      expect(result["061.945.150.735"]).to eq ["/about"]
     end
   end
 
